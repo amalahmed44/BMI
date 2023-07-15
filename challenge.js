243072a2114f930f395050e283180b7f1a6b54ab
amalali8 <amalza958@gmail.com>
2023-07-16 00:47:44 +0300

    answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculateBMI(weight , height){
+let BMI = weight / (height * height)
+return BMI
+}
+let AliWeight = 80
+let AliHeight = 1.90
+let MohamedWeight = 94
+let MohamedHieght = 1.86
+let AliBmi = calculateBMI(AliWeight , AliHeight)
+let MohamedBmi = calculateBMI(MohamedWeight , MohamedHieght)
+if(MohamedBmi>AliBmi){
+ console.log("Mohamed has higher BMI than Ali")
+}else if(AliBmi>MohamedBmi){
+console.log("Ali has a higher BMI than Mohamed")
+}else{
+    console.log("Both Ali and Mohamed have the same BMI")
+}
+
